dc23895176a553744b686b82484ddbfa38592667
Sofia Katzman <katzmansof@gmail.com>
2024-10-21 14:50:37 -0400

    add pseudo code and connect api for testing

--- code.ts
+++ code.ts
@@ -1,37 +1,65 @@
-// This plugin will open a window to prompt the user to enter a number, and
-// it will then create that many rectangles on the screen.
+"use strict";
 
-// This file holds the main code for plugins. Code in this file has access to
-// the *figma document* via the figma global object.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
 
-// This shows the HTML page in "ui.html".
 figma.showUI(__html__);
+//(Each Email can have multiple live events, Each live Event can have multiple Shows, Each Show has One Guest, One Title, One Show-Time)
+// *** 1. Call Airtable for Event Schedule ***
+// *** 2. Save Event Schedule Email details ***
+// *** 3. Generate "Pre-Schedule  Section" ***
+// *** 4. Iterate through each Event ***
+    // *** 5. Generate Live Event Date Hero ***
+        // *** 5.a. Iterate through each Show ***
+            // *** 5.b. Call Airtable for Show details ***
+            // *** 5.c. Generate "Schedule Section" ***
+// *** 6. Generate "Post-Schedule Section" ***
+// *** 7. Autolayout all nodes and title frame
 
-// Calls to "parent.postMessage" from within the HTML page will trigger this
-// callback. The callback will be passed the "pluginMessage" property of the
-// posted message.
-figma.ui.onmessage =  (msg: {type: string, count: number}) => {
-  // One way of distinguishing between different types of messages sent from
-  // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === 'create-shapes') {
-    // This plugin creates rectangles on the screen.
-    const numberOfRectangles = msg.count;
+//Typings from AT Requests:
+  //Email:
+    //hero-copy: string
+    //body-copy: string
+    //body-cta-copy: string
+    //body-cta-link: string
+    // closing-hero-copy: string
+    // closing-copy: string
+    // closing-cta-copy: string
+    // closing-cta-link: string
+    //Events: [array of shows]
+  //Events:
+    // Shows: [array]
+    // Name: string
 
-    const nodes: SceneNode[] = [];
-    for (let i = 0; i < numberOfRectangles; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
+  //Shows:
+    //  show-title: string
+    // show-guest: string
+    // show-time: string
+
+
+
+figma.ui.onmessage = (msg) => {
+    if (msg.type === 'import') {
+            console.log("you hit import !")
+            let emailData = null
+
+            // *** 1. Call Airtable for Event Schedule ***
+            fetch('https://api.airtable.com/v0/app85gJa99r7UJSLE/Schedule%20Email', {
+                method: 'GET',
+                headers: {
+                  'Authorization': `Bearer -`,
+                  'Content-Type': 'application/json'
+                }
+              })
+              .then(r => r.json())
+              .then((data) => {
+                // *** 2. Save Event Schedule Email details ***
+                emailData = data
+                console.log(emailData)
+            })
+
+
+
     }
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
-  }
-
-  // Make sure to close the plugin when you're done. Otherwise the plugin will
-  // keep running, which shows the cancel button at the bottom of the screen.
-  figma.closePlugin();
+    // Make sure to close the plugin when you're done. Otherwise the plugin will
+    // keep running, which shows the cancel button at the bottom of the screen.
+    // figma.closePlugin();
 };
